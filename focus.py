5fafa398852c97d9d59a71df47fe12773e7edd74
adoherty8 <azdoherty91@gmail.com>
2016-04-22 16:37:36 -0400

    Filled in function, testing added with plot focus to generate unfocused images and then compute their focus by the brenner metric.

--- focus.py
+++ focus.py
@@ -1,8 +1,43 @@
 import numpy as np
 import cv2
+import os
 
-def global_focus():
-    pass
+def global_focus(img):
+    '''
+    returns focus metric of image based on squared gradient
+        input: grayscale
+        return: float. representing how in focus the entire image is
+    '''
+    h,w = img.shape
+    return np.sum((img[0:h-2,:] - img[2:h,:])**2)/(1.0*h*w)
 
-def local_focus():
-    pass
+def local_focus(img):
+    Ix = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = 3)
+    Iy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = 3)
+    return Ix**2 + Iy**2
+ 
+def image_array(image_dir):
+    '''
+    generate an image array of Nxhxw of grayscale images from directory, sort on fname
+    '''
+    flist = os.listdir(image_dir)
+    flist = sorted(flist, key = lambda x: x.split('_')[1])
+    img1 = cv2.cvtColor(cv2.imread(os.path.join(image_dir, flist[0])), cv2.COLOR_BGR2GRAY)
+    h,w = img1.shape
+    img_array = np.empty((len(flist), h, w))
+    for i in range(len(flist)):
+        img_array[i,:,:] = cv2.cvtColor(cv2.imread(os.path.join(image_dir, flist[i])), cv2.COLOR_BGR2GRAY)
+        print flist[i]
+    return img_array
+        
+def test_run():
+    image_dir = 'photos/set_1'
+    flist = os.listdir(image_dir)
+    flist = sorted(flist, key = lambda x: x.split('_')[1])
+    im_list = []
+    for i in range(len(flist)):
+        im_list.append( cv2.cvtColor(cv2.imread(os.path.join(image_dir, flist[i])), cv2.COLOR_BGR2GRAY))
+        cv2.imwrite('output/{}_focus.png'.format(i), local_focus(im_list[i]))
+        
+if __name__ == "__main__":
+    test_run()
---
+++ plot_focus.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import os
+import focus
+from matplotlib import pyplot as plt
+
+def generate_blur_set(img, N):
+    '''
+    generate a blurred image set (N images) from a single in-focus image to test focus metrics.
+        input - img: grayscale image array
+        return - N - integer, number of blurred images to be generated
+    '''
+    h,w = img.shape
+    blurred_array = np.zeros((N,h,w))
+    for i in range(N):
+        blurred_array[i,:,:] = cv2.blur(img, (2 * (i+1) - 1, 2 * (i+1) - 1))
+    return blurred_array    
+def focus_set(img_array):
+    '''
+    determine global focus for each image 
+    '''
+    N,h,w = img_array.shape
+    f_vec = np.empty((N))
+    for i in range(N):
+        f_vec[i] = focus.global_focus(img_array[i])
+    return f_vec
+        
+   
+def plot_focus(f_vec, title = 'Focus'):
+    ax = plt.figure()
+    plt.plot(np.arange(len(f_vec)), f_vec, '.-')
+    plt.title(title)
+    plt.xlabel('image number')
+    plt.ylabel('focus value')
+    plt.savefig(title + ".png", dpi = 400)
+    
+def test_run(input_image):
+    blur_array = generate_blur_set(input_image, 20)
+    f_vec = focus_set(blur_array)
+    plot_focus(f_vec)
+
+def test_real(input_dir):
+    blur_array = focus.image_array(input_dir)
+    f_vec = focus_set(blur_array)
+    plot_focus(f_vec)
+
+    
+ 
+if __name__ == "__main__":
+    #test_image = 'photos\set_0\IMG_1229.JPG'
+    #img = cv2.cvtColor(cv2.imread(test_image), cv2.COLOR_BGR2GRAY)
+    #h,w = img.shape
+    #img = cv2.resize(img, dsize = (h/4, w/4))
+    #test_run(img)
+    test_real('photos/MVI_1227')
+
+
+    
